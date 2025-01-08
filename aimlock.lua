5aa28ebfae8e82caf53124d4ceedd5b1792646e4
uncybed <115887773+uncybed@users.noreply.github.com>
2025-01-08 03:28:41 +0000

    .

---
+++ aimlock.lua
@@ -0,0 +1,6 @@
+-- aimlock by cybe42
+-- loadstring(game:HttpGet('https://uncybed.github.io/aimlock.lua'))()
+--  OR
+-- loadstring(HttpGet('https://uncybed.github.io/aimlock.lua'))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/uncybed/c/refs/heads/main/aimlock.lua"))()
